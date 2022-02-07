6b83d2861649b6592bffeae5cb87e2ed98a1d912
Hamza Tahir <86909314+ProgrammingWithHT@users.noreply.github.com>
2022-02-07 05:17:03 +0500

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -3,10 +3,10 @@ using namespace std ;
 void Display(int a[],int n);
 // For Bubble sorting
 void BubbleSort (int arr[],int n){
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < (n-1); i++)  //also write 'n' but waste one step of time
     {
-         bool flag = false;  //More Optimized code
-        for (int j = 0; j < 5-i; j++)  //Optimized Code use 5-i
+        bool flag = false;  //More Optimized code
+        for (int j = 0; j < (n-1)-i; j++)  //Optimized Code use 5-i
         {
             if (arr[j] > arr[j+1])
             {
@@ -21,7 +21,7 @@ void BubbleSort (int arr[],int n){
 }
 
 int main(){
-    int arr[] = {5,2,4,8,4,1};
+    int arr[] = {5,2,4,8,4,1,4,1,5,7,3,2,5,43,2};
     int n = sizeof(arr)/sizeof(arr[0]);
     BubbleSort(arr,n);
     Display(arr,n);
